ad7552a1a4d98a35a0803c5f98a5c7e2c3b1f61e
Soojin <soojl1005@naver.com>
2021-03-22 00:01:00 +0900

    Add title, subtitle

--- Weather.js
+++ Weather.js
@@ -8,42 +8,62 @@ const weatherOptions = {
   Haze: {
     iconName: "weather-hazy",
     gradient: ["#bdc3c7", "#2c3e50"],
+    title: "Haze",
+    subtitle: "Just don't go outside",
   },
-  Thunderstrom: {
-    iconName: "weather-ligtning",
+  Thunderstorm: {
+    iconName: "weather-lightning",
     gradient: ["#e9d362", "#333333"],
+    title: "Thunderstorm",
+    subtitle: "THOR is fighting",
   },
   Drizzle: {
     iconName: "water-outline",
     gradient: ["#1c92d2", "#f2fcfe"],
+    title: "Drizzle",
+    subtitle: "How about take an umbrella? 🌂",
   },
   Rain: {
     iconName: "weather-rainy",
     gradient: ["#74ebd5", "#ACB6E5"],
+    title: "Rain",
+    subtitle: "☂ It's nice weather to listen music and feel the mood",
   },
   Snow: {
     iconName: "weather-snowy",
     gradient: ["#C9D6FF", "#E2E2E2"],
+    title: "Snow",
+    subtitle: "Do you wanna build a snow man ⛄",
   },
   Atmosphere: {
     iconName: "weather-cloudy-alert",
     gradient: ["#E6DADA", "#274046"],
+    title: "Atmosphere",
+    subtitle: "Be careful when you drive",
   },
   Clear: {
     iconName: "weather-sunny",
     gradient: ["#F2994A", "#F2C94C"],
+    title: "Clear",
+    subtitle: "It's so lovely day 🥰",
   },
   Clouds: {
     iconName: "weather-cloudy",
     gradient: ["#076585", "#fff"],
+    title: "Cloud",
+    subtitle: "Take a picture of clouds",
   },
   Mist: {
     iconName: "weather-fog",
     gradient: ["#B993D6", "#8CA6DB"],
+    title: "Mist",
+    subtitle: "I think it's not a good weather",
   },
   Dust: {
     iconName: "smog",
     gradient: ["#606c88", "#3f4c6b"],
+    title: "Dust",
+    subtitle: "I recommend you to wear a mask",
   },
 };
 
@@ -62,7 +82,12 @@ export default function Weather({ temp, condition }) {
         />
         <Text style={styles.temp}>{temp}°</Text>
       </View>
-      <View style={styles.halfContainer} />
+      <View style={{ ...styles.halfContainer, ...styles.textContainer }}>
+        <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+        <Text style={styles.subtitle}>
+          {weatherOptions[condition].subtitle}
+        </Text>
+      </View>
     </LinearGradient>
   );
 }
@@ -70,7 +95,7 @@ export default function Weather({ temp, condition }) {
 Weather.propTypes = {
   temp: PropTypes.number.isRequired,
   condition: PropTypes.oneOf([
-    "Thunderstrom",
+    "Thunderstorm",
     "Drizzle",
     "Rain",
     "Snow",
@@ -97,5 +122,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  title: {
+    color: "white",
+    fontSize: 54,
+    fontWeight: "300",
+    marginBottom: 10,
+  },
+
+  subtitle: {
+    fontWeight: "600",
+    color: "white",
+    fontSize: 24,
+  },
+  textContainer: {
+    paddingHorizontal: 20,
+    alignItems: "flex-start",
   },
 });
